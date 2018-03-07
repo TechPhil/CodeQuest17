566ded1e3e8e60345578173f9bf64e3c65e9afc2
TechPhil <philstergaming@gmail.com>
2018-03-07 13:29:41 +0000

    Added Python Folder for Solutions
    Copy of Example and Judging Inputs required for ease of use within programs.
    How file paths are tackled this year depends on how the setup on the day is.
    
    Co-Authored-By: maxpowell123 <maxpowell123@users.noreply.github.com>

---
+++ Python/Problem 1.py
@@ -0,0 +1,9 @@
+file=open("ExampleInputs\Prob01.in.txt","r")
+lines=file.readlines()
+##print (lines)
+lines.pop(0)
+##print (lines)
+for line in lines:
+    line=line.strip()
+    print(line)
+    print(line)
